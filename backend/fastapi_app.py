fd4b62f6c40e941e28bf2fb45d33033d0d5b4c49
coolCatalyst <jjay44480@gmail.com>
2023-12-06 17:53:37 +0000

    fix: cors error

--- backend/fastapi_app.py
+++ backend/fastapi_app.py
@@ -42,6 +42,7 @@ origins = [
     "http://localhost",
     "http://localhost:3000",
     "http://localhost:3001",
+    "https://wishart-support-bot-frontend-ai-general1s-projects.vercel.app/"
 ]
 
 app.add_middleware(
